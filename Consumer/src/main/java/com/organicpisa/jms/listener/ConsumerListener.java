988c0b09f6a76e57fe6c8c342436c164b12e46a2
Vinh Tran <vinhtran0910@gmail.com>
2015-12-21 14:58:12 -0800

    Baseline checkin

---
+++ Consumer/src/main/java/com/organicpisa/jms/listener/ConsumerListener.java
@@ -0,0 +1,16 @@
+package com.organicpisa.jms.listener;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerListener implements MessageListener{
+
+	public void onMessage(Message arg0) {
+		System.out.println("In onMessage()");
+		
+	}
+
+}
