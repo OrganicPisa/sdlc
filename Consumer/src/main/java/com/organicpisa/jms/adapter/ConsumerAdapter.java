8d5aee42ddddee98cef3e529e8a9055f137df430
Vinh Tran <vinhtran0910@gmail.com>
2015-12-21 16:25:05 -0800

    added adapter and junit test

---
+++ Consumer/src/main/java/com/organicpisa/jms/adapter/ConsumerAdapter.java
@@ -0,0 +1,34 @@
+package com.organicpisa.jms.adapter;
+
+import java.net.UnknownHostException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.util.JSON;
+
+
+@Component
+public class ConsumerAdapter {
+
+	private static Logger logger = LogManager.getLogger(ConsumerAdapter.class.getName());
+	
+	@SuppressWarnings({ "deprecation", "resource" })
+	public void sendToMongo(String json) throws UnknownHostException{
+		logger.info("Sending to MongoDB");
+		MongoClient client = new MongoClient();
+		DB db = client.getDB("vendor");
+		DBCollection collection = db.getCollection("contact");
+		logger.info("Convert JSON string to DB Object");
+		
+		DBObject object = (DBObject)JSON.parse(json);
+		collection.insert(object);
+		logger.info("Sent to MongoDB");		
+	}
+
+}
--- Consumer/src/main/java/com/organicpisa/jms/listener/ConsumerListener.java
+++ Consumer/src/main/java/com/organicpisa/jms/listener/ConsumerListener.java
@@ -1,16 +1,52 @@
 package com.organicpisa.jms.listener;
 
+import java.net.UnknownHostException;
+
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
+
+import com.organicpisa.jms.adapter.ConsumerAdapter;
 
 @Component
 public class ConsumerListener implements MessageListener{
 
-	public void onMessage(Message arg0) {
-		System.out.println("In onMessage()");
-		
+	private static Logger logger = LogManager.getLogger(ConsumerListener.class.getName());
+	
+	@Autowired
+	JmsTemplate jmstemplate ;	
+	@Autowired
+	ConsumerAdapter consumeradapter;
+	
+	
+	public void onMessage(Message message) {
+		logger.info("In onMessage()");
+		String json = null;
+		if(message instanceof TextMessage){
+			try {
+				json = ((TextMessage)message).getText();
+				logger.info("Sending JSON to db: "+ json);
+				consumeradapter.sendToMongo(json);
+			} catch (JMSException e) {
+				logger.error("message : "+json);
+				jmstemplate.convertAndSend(json);
+			}catch (UnknownHostException e) {
+				logger.error("message : "+json);
+				jmstemplate.convertAndSend(json);
+			}catch (Exception e) {
+				logger.error("message : "+json);
+				jmstemplate.convertAndSend(json);
+			}
+		}
 	}
+	
+	
 
 }
